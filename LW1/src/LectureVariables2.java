3c213223cf8930f094bcf7d006722f9d19e7ddaa
David Schuster <searay330@imageknight.com>
2017-03-03 02:01:30 -0500

    added lecture week 1 solutions

---
+++ LW1/src/LectureVariables2.java
@@ -0,0 +1,72 @@
+public class LectureVariables2 {
+
+
+
+    // 2 points
+    static int Q1() {
+        int first = 60;
+        int second = 4;
+
+        // TODO: Edit the following code to set the value of result to the addition of first and second
+        // Do not change the values stored in first or second
+        int result = first + second;
+
+
+        return result;
+    }
+
+
+
+    // 2 points
+    static double Q2() {
+        double first = 1.68;
+        double second = 0.47;
+
+        // TODO: Edit the following code to set the value of result to the multiplication of first and second
+        // Do not change the values stored in first or second
+        double z = first * second;
+
+
+        return z;
+    }
+
+
+
+
+    // 2 points
+    static void Q3() {
+        int container = 416;
+        // TODO: Print "container is storing the value " followed by the value of number on the same line.
+        // Note the space after the word value  (Do not change the value of container)
+        System.out.println("container is storing the value " + container);
+
+    }
+
+
+
+
+    // 2 points
+    static String Q4() {
+        String first = "I understand";
+        String second = "String concatenation!";
+
+        // TODO: set result equal to the concatenation of first and second separated by a space
+        // Do not change the values of first or second
+        String result = first + " " + second;
+
+
+        return result;
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // You can optionally use this main method to test your code without having to submit
+        // This main method will not be graded
+
+    }
+
+}
+
+
---
+++ LW1/src/Lecture_Variables.java
@@ -0,0 +1,41 @@
+public class Lecture_Variables {
+
+
+
+    // 2 points
+    static int Q1() {
+        int number = 42;
+        // TODO: set the value of number to 42
+
+
+        return number;
+    }
+
+
+    // 2 points
+    static double Q2() {
+        double number = 1.7;
+        // TODO: set the value of number to 1.7
+
+
+        return number;
+    }
+
+
+    // 3 points
+    static void Q3() {
+        // TODO: print "Welcome to CSE115"
+    	System.out.println("Welcome to CSE115");
+
+    }
+
+
+
+    public static void main(String[] args) {
+
+        // You can optionally use this main function to test your code without having to submit
+        // This main function will not be graded
+
+    }
+
+}
