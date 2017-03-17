1e26148fbb9fe688f3609cf4ed155f7f67533d3b
David Schuster <searay330@imageknight.com>
2017-03-17 01:33:38 -0400

    added lecture week 6 day 3

---
+++ LW6/src/AlgorithmsLecture3.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class AlgorithmsLecture3 {
+
+
+    // 5 points
+    static void Q1(ArrayList<Integer> input) {
+
+        // sort the input ArrayList in increasing order
+        //
+        // Hint: Do not overthink this one! Today will be a traditional lecture that
+        //       will take 50 minutes. Get your points, sit back, and enjoy lecture.
+    	Collections.sort(input);
+    }
+
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
